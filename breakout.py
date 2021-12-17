086028e69910bf84ed8473b235d3e93610e77f65
Mike Huang <MikeH_Huang@compal.com>
2021-12-17 10:09:52 +0800

    <NEW><FIRSTFILE>
     @Upload first file
    	new file:   __MACOSX/._breakout.py
    	new file:   __MACOSX/._breakoutgraphics.py
    	new file:   breakout.py
    	new file:   breakoutgraphics.py

---
+++ breakout.py
@@ -0,0 +1,24 @@
+"""
+stanCode Breakout Project
+Adapted from Eric Roberts's Breakout by
+Sonja Johnson-Yu, Kylie Jue, Nick Bowman,
+and Jerry Liao.
+
+YOUR DESCRIPTION HERE
+"""
+
+from campy.gui.events.timer import pause
+from breakoutgraphics import BreakoutGraphics
+
+FRAME_RATE = 1000 / 120  # 120 frames per second
+NUM_LIVES = 3			 # Number of attempts
+
+
+def main():
+    graphics = BreakoutGraphics()
+
+    # Add the animation loop here!
+
+
+if __name__ == '__main__':
+    main()
---
+++ breakoutgraphics.py
@@ -0,0 +1,43 @@
+"""
+stanCode Breakout Project
+Adapted from Eric Roberts's Breakout by
+Sonja Johnson-Yu, Kylie Jue, Nick Bowman, 
+and Jerry Liao.
+
+YOUR DESCRIPTION HERE
+"""
+from campy.graphics.gwindow import GWindow
+from campy.graphics.gobjects import GOval, GRect, GLabel
+from campy.gui.events.mouse import onmouseclicked, onmousemoved
+import random
+
+BRICK_SPACING = 5      # Space between bricks (in pixels). This space is used for horizontal and vertical spacing
+BRICK_WIDTH = 40       # Height of a brick (in pixels)
+BRICK_HEIGHT = 15      # Height of a brick (in pixels)
+BRICK_ROWS = 10        # Number of rows of bricks
+BRICK_COLS = 10        # Number of columns of bricks
+BRICK_OFFSET = 50      # Vertical offset of the topmost brick from the window top (in pixels)
+BALL_RADIUS = 10       # Radius of the ball (in pixels)
+PADDLE_WIDTH = 75      # Width of the paddle (in pixels)
+PADDLE_HEIGHT = 15     # Height of the paddle (in pixels)
+PADDLE_OFFSET = 50     # Vertical offset of the paddle from the window bottom (in pixels)
+INITIAL_Y_SPEED = 7    # Initial vertical speed for the ball
+MAX_X_SPEED = 5        # Maximum initial horizontal speed for the ball
+
+
+class BreakoutGraphics:
+
+    def __init__(self, ball_radius=BALL_RADIUS, paddle_width=PADDLE_WIDTH, paddle_height=PADDLE_HEIGHT,
+                 paddle_offset=PADDLE_OFFSET, brick_rows=BRICK_ROWS, brick_cols=BRICK_COLS, brick_width=BRICK_WIDTH,
+                 brick_height=BRICK_HEIGHT, brick_offset=BRICK_OFFSET, brick_spacing=BRICK_SPACING, title='Breakout'):
+
+        # Create a graphical window, with some extra space
+        window_width = brick_cols * (brick_width + brick_spacing) - brick_spacing
+        window_height = brick_offset + 3 * (brick_rows * (brick_height + brick_spacing) - brick_spacing)
+        self.window = GWindow(width=window_width, height=window_height, title=title)
+
+        # Create a paddle
+        # Center a filled ball in the graphical window
+        # Default initial velocity for the ball
+        # Initialize our mouse listeners
+        # Draw bricks
